57ca734e09964d700c4a09f277bcc2954889f7aa
Hannes Palmquist <hannes.palmquist@outlook.com>
2023-11-16 20:02:52 +0100

    Added class for MQTTMessage

---
+++ source/Classes/PSMQTTMessage.class.ps1
@@ -0,0 +1,40 @@
+﻿class PSMQTTMessage
+{
+    [string]$Topic
+    [string]$Payload
+    [byte[]]$PayloadUTF8ByteA
+    [datetime]$Timestamp
+    [boolean]$DupFlag
+    [int]$QosLevel
+    [boolean]$Retain
+
+    PSMQTTMessage(
+        [string]$Topic,
+        [string]$Payload
+    )
+    {
+        $this.Topic = $Topic
+        $this.Payload = $Payload
+        $this.PayloadUTF8ByteA = [System.Text.Encoding]::UTF8.GetBytes($Payload)
+        $this.Timestamp = (Get-Date)
+    }
+
+    PSMQTTMessage(
+        [System.Management.Automation.PSEventArgs]$EventObject
+    )
+    {
+        $this.Topic = $EventObject.SourceEventArgs.Topic
+        $this.Payload = [System.Text.Encoding]::ASCII.GetString($EventObject.SourceEventArgs.Message)
+        $this.PayloadUTF8ByteA = $EventObject.SourceEventArgs.Message
+        $this.DupFlag = $EventObject.SourceEventArgs.DupFlag
+        $this.QosLevel = $EventObject.SourceEventArgs.QosLevel
+        $this.Retain = $EventObject.SourceEventArgs.Retain
+        $this.Timestamp = $EventObject.TimeGenerated
+    }
+
+    [string] ToString ()
+    {
+        return ($this.Topic + ';' + $this.Payload)
+    }
+
+}
