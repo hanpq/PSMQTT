78bbb13ba3d3f823ad4b523742b4378a8e8dedeb
Hannes Palmquist <hannes.palmquist@outlook.com>
2023-11-16 20:02:25 +0100

    Added import of MQTTMessage format data

--- source/prefix.ps1
+++ source/prefix.ps1
@@ -1,2 +1,7 @@
 ﻿# The content of this file will be prepended to the top of the psm1 module file. This is useful for custom module setup is needed on import.
+
+# Add M2Mqtt dependency library
 Add-Type -Path "$PSScriptRoot\Include\M2Mqtt.Net.dll"
+
+# Import custom formatting
+Update-FormatData -AppendPath "$PSScriptRoot\Include\PSMQTT.Format.ps1xml"
